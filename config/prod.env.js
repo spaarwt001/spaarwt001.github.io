0002020f246c23c02ea94b3e50a4204338654285
Jun <junske@live.nl>
2018-10-28 11:52:48 +0100

    test

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  baseUrl: process.env.NODE_ENV === '/spaarwt001/'
+  baseUrl: process.env.NODE_ENV === 'spaarwt001.github.io'
   ? '/spaarwt001.github.io/'
   : '/'
 }
